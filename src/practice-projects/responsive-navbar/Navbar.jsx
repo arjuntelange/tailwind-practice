28650e9b0ae2a9a254e89455fd9566ad3bc1f798
Arjun Telange <arjuntelange08@gmail.com>
2026-07-19 14:50:08 +0530

    feat(navbar): add responsive mobile dropdown navigation

--- src/practice-projects/responsive-navbar/Navbar.jsx
+++ src/practice-projects/responsive-navbar/Navbar.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import { Menu } from "lucide-react";
+import React, { useState } from "react";
 
 function Navbar() {
   const navlink =
-    "px-2 py-0.5 rounded-xl text-blue-400 hover:text-blue-500 hover:scale-105 hover:bg-blue-100 transition-all duration-200 ease-in";
+    "px-2 py-0.5 rounded-xl text-gray-400 flex items-center hover:text-gray-500 hover:scale-105 hover:bg-gray-100 transition-all duration-200 ease-in";
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-      <nav className="w-full h-16 px-16 flex justify-between items-center shadow-md">
+      <nav className="w-full h-16 px-6 md:px-16 flex justify-between items-center shadow-md relative">
         <div className="font-extrabold text-slate-800 text-3xl">🤖Logo</div>
-        <ul className="flex items-center gap-12">
+        <div className="block md:hidden">
+          <button
+            aria-label="Open navigation menu"
+            onClick={() => setIsOpen((prev) => !prev)}
+          >
+            <Menu />
+          </button>
+        </div>
+        <ul
+          className={`
+            ${isOpen ? "flex flex-col gap-2 p-4 mt-4 min-w-40 rounded-2xl bg-white shadow-lg absolute top-full right-4" : "hidden"}
+            md:flex
+            md:flex-row
+            md:gap-8
+            `}
+        >
           <li>
             <a href="#" className={navlink}>
               Home
